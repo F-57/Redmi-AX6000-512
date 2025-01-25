2422f0ea583f0f81c7ed37d5f6e65924a93851c6
caiweill <80924478+caiweill@users.noreply.github.com>
2025-01-26 02:09:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,13 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# ---------------------------------------------------------------
-## drop mosdns 和 v2ray-geodata
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# ---------------------------------------------------------------
 ## OpenClash
 # git clone -b v0.45.141-beta --depth=1 https://github.com/vernesong/openclash.git OpenClash
 git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
