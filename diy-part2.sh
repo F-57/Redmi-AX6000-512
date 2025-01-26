9a3daecbd3c245f4c1b4358aef44462fb3e1e050
caiweill <80924478+caiweill@users.noreply.github.com>
2025-01-26 12:27:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,6 +30,7 @@ rm -rf package/feeds/luci/luci-app-openclash
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # ---------------------------------------------------------------
 ## OpenClash
