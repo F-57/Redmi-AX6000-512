15581583606707d23ac10961c48f12099aa42eaa
caiweill <80924478+caiweill@users.noreply.github.com>
2025-02-09 21:40:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,8 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# echo "CONFIG_PACKAGE_luci-i18n-autotimeset-zh-cn=y" >> .config
+
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
