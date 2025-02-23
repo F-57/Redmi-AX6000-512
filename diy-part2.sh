e0e9f25b8eefa3d61fdc94ec33139a23d50ee3da
caiweill <80924478+caiweill@users.noreply.github.com>
2025-02-24 03:05:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,9 +41,6 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 ## 获取隔空播放luci-app-airconnect
 git clone https://github.com/sbwml/luci-app-airconnect package/airconnect
 
-## 获取ddns-go
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-
 ## OpenClash
 git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
 rm -rf feeds/luci/applications/luci-app-openclash
@@ -74,7 +71,7 @@ git clone https://github.com/caiweill/luci-app-lucky package/lucky
 # 软件包与配置
 
 # 主题
-#echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
 # 终端
 #echo "CONFIG_PACKAGE_luci-app-ttyd=y" >> .config
 # 释放内存
