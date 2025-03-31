ce98cd0a177527467341f05f64c693ba9fac3cd2
caiweill <80924478+caiweill@users.noreply.github.com>
2025-04-01 07:53:05 +0800

    Update padavanonly_24.10_part2.sh

--- padavanonly_24.10_part2.sh
+++ padavanonly_24.10_part2.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-# 主机
-WRT_NAME="AX6000"
 WRT_IP="10.0.0.1"
-# Wi-Fi
-WRT_SSID="Ax6000"
-WRT_WORD="cw010203"
 
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#修改immortalwrt.lan关联IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-#添加编译日期标识
-sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
-
-WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
-#修改WIFI名称
-sed -i "s/ImmortalWrt/$WRT_SSID/g" $WIFI_FILE
-#修改WIFI加密
-sed -i "s/encryption=.*/encryption='sae-mixed'/g" $WIFI_FILE
-#修改WIFI密码
-sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='$WRT_WORD'" $WIFI_FILE
+WRT_NAME="AX6000"
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
-#修改默认IP地址
+
+#修改immortalwrt.lan关联IP
+
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+
+#修改默认IP地址 修改默认主机名
+
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
-#修改默认主机名
+
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+
+WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
+
+WIFI_SSID="Ax6000"
+
+WIFI_PASS="cw010203"
+
+#修改WIFI名称 修改WIFI加密 修改WIFI密码
+
+sed -i "s/ImmortalWrt/$WIFI_SSID/g" $WIFI_FILE
+
+sed -i "s/encryption=.*/encryption='sae-mixed'/g" $WIFI_FILE
+
+sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='$WIFI_PASS'" $WIFI_FILE
+
