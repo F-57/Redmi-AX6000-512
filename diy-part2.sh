f47a012b4ee3e524326d81658e28a31af5147264
楠方 <80924478+F-57@users.noreply.github.com>
2025-04-19 15:49:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -147,9 +147,6 @@ echo -e "msgstr \"大吉大利\"" >> package/lucky/luci-app-lucky/po/zh_Hans/luc
 echo -e "\nmsgid \"AList\"" >> feeds/luci/applications/luci-app-alist/po/zh_Hans/alist.po
 echo -e "msgstr \"聚合网盘\"" >> feeds/luci/applications/luci-app-alist/po/zh_Hans/alist.po
 
-#echo -e "\nmsgid \"Tailscale\"" >> package/luci-app-tailscale/po/zh_Hans/tailscale.po
-#echo -e "msgstr \"虚拟组网\"" >> package/luci-app-tailscale/po/zh_Hans/tailscale.po
-
 echo -e "\nmsgid \"WireGuard\"" >> feeds/luci/applications/luci-app-wireguard/po/zh_Hans/wireguard.po
 echo -e "msgstr \"异地组网\"" >> feeds/luci/applications/luci-app-wireguard/po/zh_Hans/wireguard.po
 
