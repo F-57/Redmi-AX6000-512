a654c716b2f804a95caf44cbbb397465e137c718
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-01 02:26:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,7 +46,7 @@ git clone https://github.com/F-57/luci-app-adguardhome package/luci-app-adguardh
 
 # 安装 mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/mosdns
 rm -rf package/feeds/packages/mosdns
