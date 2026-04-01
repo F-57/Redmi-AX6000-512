0706f3ed22bf09fd708a746a5e2fbf493a7fca5b
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-02 02:35:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -97,10 +97,10 @@ sed -i 's/services/network/g' package/mtk/applications/luci-app-eqos-mtk/root/us
 echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-ttyd=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-autoreboot=y" >> .config
-# echo "CONFIG_PACKAGE_luci-app-mwan3=y" >> .config
-# echo "CONFIG_PACKAGE_luci-app-syncdial=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-mwan3=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-syncdial=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
-# echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-lucky=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-airconnect=y" >> .config
