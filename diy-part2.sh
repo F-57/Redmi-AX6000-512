b4f686f3954dc83fec7a51abc51a0990750b864d
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-11 02:16:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,12 +13,12 @@ WRT_IP="10.0.0.1"
 WRT_NAME="AX6000"
 CFG_FILE="./package/base-files/files/bin/config_generate"
 
-#修改immortalwrt.lan关联IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-
 #修改默认IP地址 修改默认主机名
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+
+#修改immortalwrt.lan关联IP
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 
 #修改中文显示
 #sed -i 's/auto/zh_cn/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base
